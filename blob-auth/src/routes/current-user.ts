9f6d363ea3311939d35bf8cc71fa8177d222de21
anawatj <ajarusiripot@gmail.com>
2024-09-15 19:54:49 +0700

    auth flow

--- blob-auth/src/routes/current-user.ts
+++ blob-auth/src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { requireAuth } from '@taoblob/commons';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser,requireAuth, (req, res) => {
+router.get('/api/users/currentuser', currentUser, (req, res) => {
   
   res.send({ currentUser: req.currentUser || null });
 });
